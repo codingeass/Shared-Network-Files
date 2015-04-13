2c3a27b83256f088e57b3b99322ece6b9fe0b7f8
codingeass <amandeeptheviper@gmail.com>
2015-04-13 11:38:21 +0530

    added files of the app

---
+++ Network Search/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(25, 396)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(561, 169)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(157, 367)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(25, 571)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(561, 23)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(25, 370)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(12, 12)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(328, 325)
+        Me.TreeView1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(611, 606)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+
+End Class
---
+++ Network Search/Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports System.IO
+Imports System.DirectoryServices
+Imports System.Threading
+Imports System.Net
+Imports System.Text.RegularExpressions
+Public Class Form1
+    Dim thread As Thread
+    Public Function GetHostName() As String
+        Return Dns.GetHostName()
+    End Function
+    Shared Function GetProcessText(ByVal process As String, ByVal param As String, ByVal workingDir As String) As String
+        Dim p As Process = New Process
+        ' this is the name of the process we want to execute 
+        p.StartInfo.FileName = process
+        If Not (workingDir = "") Then
+            p.StartInfo.WorkingDirectory = workingDir
+        End If
+        p.StartInfo.Arguments = param
+        ' need to set this to false to redirect output
+        p.StartInfo.UseShellExecute = False
+        p.StartInfo.RedirectStandardOutput = True
+        ' start the process 
+        p.Start()
+        ' read all the output
+        ' here we could just read line by line and display it
+        ' in an output window 
+        Dim output As String = p.StandardOutput.ReadToEnd
+        ' wait for the process to terminate 
+        p.WaitForExit()
+        Return output
+    End Function
+    Public Function GetIpAddress() As String
+        Return Dns.GetHostEntry(GetHostName()).AddressList(0).ToString()
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FindingThreats()
+        Me.BackColor = Color.DarkSlateGray
+        Dim result As String = GetProcessText("arp", "-a", "")
+        'MsgBox(result)
+        'Dim regex As Regex = New Regex("(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])", RegexOptions.IgnoreCase)
+        'Dim match As Match = regex.Match(result)
+        'MsgBox(match.Groups(1).Value & "hello")
+        Dim res() As String = result.Split(New String() {Environment.NewLine},StringSplitOptions.RemoveEmptyEntries)
+        Dim res1() As String
+        Dim i As Integer
+        'Dim i As Integer
+        'Dim str As String = ""
+        'While i < res1.Length
+        '    str = str + " " + res1(i) + "=" + i.ToString
+        '    i += 1
+        'End While
+
+        'MsgBox(res1(2))
+        'Tree code
+        Dim root = New TreeNode("Network")
+        TreeView1.Nodes.Add(root)
+        Dim treeIp As TreeNode
+        i = 3
+        While i < res.Length
+            'TreeView1.Nodes.Clear()
+            res1 = Split(res(i))
+            If (res1(2)(0) <> "2") Then
+                Try
+                    treeIp = New TreeNode(res1(2))
+                    treeIp.Name = res1(2)
+                    treeIp.Text = res1(2)
+                    TreeView1.Nodes(0).Nodes.Add(treeIp)
+                    'EnterTheFolder(res1(2), treeIp)
+                    Dim thread1 As Thread = New System.Threading.Thread(Sub() Me.EnterTheFolder(res1(2), treeIp))
+                    thread1.Start()
+                    'TreeView1.Nodes(0).Nodes.Add(New TreeNode(System.Net.Dns.GetHostEntry(res1(2)).HostName.ToString))
+                Catch
+                End Try
+                'Tree code End
+            End If
+            i += 1
+        End While
+        treeIp = New TreeNode("172.17.220.200")
+        TreeView1.Nodes(0).Nodes.Add(treeIp)
+        EnterTheFolder("172.17.220.200", treeIp)
+        TextBox1.Text = GetHostName()
+    End Sub
+
+    Sub EnterTheFolder(ipAddress As String, treeIp As TreeNode)
+        Dim result As String = GetProcessText("net", "view " & ipAddress, "")
+        Dim res() As String = result.Split(New String() {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+        Dim i As Integer = 0
+        'Dim str As String = ""
+        Dim counter As Integer = 0
+        While i < res.Length
+            If (Trim(res(i)).Split().Last = "Disk") Then
+                Try
+                    'MsgBox(TreeView1.Nodes(0).Nodes(0).FullPath)
+                    'Try
+                    '    If (counter = 0) Then
+                    '        Dim arr As TreeNode() = TreeView1.Nodes.Find(ipAddress, True)
+                    '        If arr.Length <> 0 Then
+                    '            Return
+                    '        End If
+                    '        If TreeView1.InvokeRequired And arr.Length = 0 Then
+                    '            MsgBox(arr.Length)
+                    '            TreeView1.Invoke(DirectCast(Sub() TreeView1.Nodes(0).Nodes.Add(treeIp), MethodInvoker))
+                    '        End If
+                    '    End If
+                    'Catch
+                    'End Try
+                    If TreeView1.InvokeRequired Then
+                        TreeView1.Invoke(DirectCast(Sub() TreeView1.Nodes(0).Nodes(TreeView1.Nodes(0).Nodes.IndexOf(treeIp)).Nodes.Add(New TreeNode(Trim(res(i).Replace("Disk", "")))), MethodInvoker))
+                    End If
+                    counter = 1
+                    'TreeView1.Nodes(0).Nodes(TreeView1.Nodes(0).Nodes.IndexOf(treeIp)).Nodes.Add(New TreeNode(Trim(res(i).Replace("Disk", ""))))
+                Catch m As Exception
+                    MsgBox(m.Message)
+                End Try
+                'str += Trim(res(i).Replace("Disk", ""))
+            End If
+            i += 1
+        End While
+    End Sub
+
+    Sub FindingThreats()
+        ListView1.Items.Clear()
+        Dim childEntry As DirectoryEntry
+        Dim ParentEntry As New DirectoryEntry
+        Try
+            ParentEntry.Path = "WinNT:"
+            For Each childEntry In ParentEntry.Children
+                Select Case childEntry.SchemaClassName
+                    Case "Domain"
+                        Dim SubChildEntry As DirectoryEntry
+                        Dim SubParentEntry As New DirectoryEntry
+                        SubParentEntry.Path = "WinNT://" & childEntry.Name
+                        For Each SubChildEntry In SubParentEntry.Children
+                            Select Case SubChildEntry.SchemaClassName
+                                Case "Computer"
+                                    ListView1.Items.Add(SubChildEntry.Name)
+
+                            End Select
+                        Next
+                End Select
+            Next
+        Catch Excep As Exception
+            MsgBox("Error While Reading Directories : " + Excep.Message.ToString)
+        Finally
+            ParentEntry = Nothing
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'thread = New System.Threading.Thread(AddressOf FindingHost)
+        'thread.Start()
+        Dim strHostName As String = "aditya"
+        Dim strIPAddress As String = ""
+        'MsgBox(Dns.GetHostByAddress("172.17.221.10"))
+        Dim objAddressList() As System.Net.IPAddress = _
+            System.Net.Dns.GetHostEntry(strHostName).AddressList
+        For x = 0 To objAddressList.GetUpperBound(0)
+            If objAddressList(x).AddressFamily = Net.Sockets.AddressFamily.InterNetwork Then
+                strIPAddress = objAddressList(x).ToString
+                Exit For
+            End If
+        Next
+        RichTextBox1.AppendText(strIpAddress)
+    End Sub
+    Sub FindingHost()
+        For i = 1 To 254
+            Try
+                RichTextBox1.AppendText("Host name =  " & System.Net.Dns.GetHostEntry("172.17.220." & i.ToString).HostName.ToString & vbCrLf & vbCrLf)
+            Catch ex As Exception
+            End Try
+        Next
+    End Sub
+End Class
